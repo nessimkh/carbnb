09a5f56ca92fbb20967f162c642c00656a54a9db
sorina tudor <sorinatudor94@gmail.com>
2023-08-22 11:34:50 +0200

    use index.html.erb as homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :vehicles
+  has_many :rentals
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "vehicles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resource :vehicle, only: [:index, :new, :create, :show]
+  resource :rentals, only: [:show]
 end
