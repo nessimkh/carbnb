607a9c41f036c20c96caa34b0567caeab779a5ad
sorina tudor <sorinatudor94@gmail.com>
2023-08-22 14:00:16 +0200

    rental is working

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,4 +2,13 @@ class VehiclesController < ApplicationController
   def index
     @vehicles= Vehicle.all
   end
+
+  def show
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  private
+  def veh_params
+  params.require(:vehicle).permit(:model, :transmission, :power_source)
+  end
 end
