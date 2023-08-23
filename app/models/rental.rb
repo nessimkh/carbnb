65e85cee5d37a0256b187f49466b0ad3708ba7d2
sorina tudor <sorinatudor94@gmail.com>
2023-08-23 11:41:17 +0200

    new studd

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,20 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
+  validates :end_time, comparison: { greater_than: :start_time }
+  validates :date_range_available?
+ #TODO implement block for certain vehicle at the time slot already rented
+  def date_range_available?
+    # https://stackoverflow.com/a/36354313
+    rental_events = Rental.where(
+      '(start_time BETWEEN ? AND ?) OR (end_time BETWEEN ? AND ?) OR (start_time <= ? AND end_time >= ?)',
+      start_time,
+      end_time,
+      start_time,
+      end_time,
+      start_time,
+      end_time
+    )
+    rental_events.empty?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,9 @@ User.destroy_all
 
 puts "Creating new Vehicles, User & Rentals whuuu"
 
-# Users
-sorina = User.create(
-  first_name: "Sorina",
-  last_name: "Tudor",
-  email: "sorina@test.com",
-  password: "123456"
-)
-
-nessim = User.create(
-  first_name: "Nessim",
-  last_name: "Khayyat",
-  email: "nessim@test.com",
-  password: "123456"
-)
-
 
 # Vehicles
-100.times do
+12.times do
   vehicle = Vehicle.create!(
   model: Faker::Vehicle.make_and_model,
   transmission: Faker::Vehicle.transmission,
@@ -31,22 +16,9 @@ nessim = User.create(
   doors: Faker::Vehicle.door_count,
   kilometer: Faker::Vehicle.kilometrage,
   year: Faker::Vehicle.year,
-  license_plate: Faker::Vehicle.license_plate
+  license_plate: Faker::Vehicle.license_plate,
+  standard_km_rate: "0.98€/km"
   )
 end
-
-sorinas_car = Vehicle.create(
-  model: "Renault",
-  transmission: "manuel",
-  power_source: "stick",
-  user_id: sorina.id)
-
-
-  # Rentals
-Rental.create(
-  start_time: "21/08/2023",
-  end_time: "25/08/2023",
-  vehicle: sorinas_car,
-  user: nessim)
 
 puts "Done!!"
